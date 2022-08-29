53ba9cfeac552d6af1ba79ceb33d003f3d248a2b
Lívia Cardoso <livia.cardoso@superior-ind.com>
2022-08-29 16:35:05 -0300

    Display of calculator

---
+++ src/components/Display.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {StyleSheet, Text, SafeAreaView} from 'react-native';
+
+export default props => (
+  <SafeAreaView style={styles.display}>
+    <Text style={styles.displayValue} numberOfLines={1}>
+      {props.value}
+    </Text>
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  display: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.6)',
+    alignItems: 'flex-end',
+  },
+
+  displayValue: {
+    fontSize: 60,
+    color: '#FFF',
+  },
+});
