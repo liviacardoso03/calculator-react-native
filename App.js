cdc63a0932a1703a743826a217e178ed3b7d5e36
Lívia Cardoso <livia.cardoso@superior-ind.com>
2022-08-29 16:34:49 -0300

    Display of calculator

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
-import {Platform, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import Button from './src/components/Button';
+import Display from './src/components/Display';
 
 export default class App extends Component {
+  state = {
+    displayValue: '0',
+  };
+
   render() {
     return (
       <SafeAreaView style={styles.container}>
+        <Display value={this.state.displayValue} />
         <SafeAreaView style={styles.buttons}>
           <Button label="AC" />
           <Button label="/" />
