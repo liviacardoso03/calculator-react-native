8cb4112216b592a92c6fa428765383a4e1082dd0
Lívia Cardoso <livia.cardoso@superior-ind.com>
2022-08-30 12:13:42 -0300

    Buttons of calculator

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,23 @@ import React from 'react';
 import {StyleSheet, Text, Dimensions, TouchableHighlight} from 'react-native';
 
 export default props => {
+  const stylesButton = [styles.buttons];
+
+  if (props.double) {
+    stylesButton.push(styles.buttonDouble);
+  }
+
+  if (props.triple) {
+    stylesButton.push(styles.buttonTriple);
+  }
+
+  if (props.operation) {
+    stylesButton.push(styles.operationButton);
+  }
+
   return (
-    <TouchableHighlight onPress={props.onClick}>
-      <Text style={styles.buttons}>{props.label}</Text>
+    <TouchableHighlight onPress={() => props.onClick(props.label)}>
+      <Text style={stylesButton}>{props.label}</Text>
     </TouchableHighlight>
   );
 };
@@ -19,5 +33,18 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     borderWidth: 1,
     borderColor: '#888',
+  },
+
+  operationButton: {
+    color: '#fff',
+    backgroundColor: '#fa8231',
+  },
+
+  buttonDouble: {
+    width: (Dimensions.get('window').width / 4) * 2,
+  },
+
+  buttonTriple: {
+    width: (Dimensions.get('window').width / 4) * 3,
   },
 });
